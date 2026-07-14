35dd9cdfef1253e2dc8805bf00b417f98bf07807
tahayassine2000najjar-droid <tahayassine2000najjar@gmail.com>
2026-07-14 13:30:06 +0100

    feat: add MongoDB connection utility

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+async function connectDB() {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  await mongoose.connect(MONGODB_URI);
+}
+
+export default connectDB;
