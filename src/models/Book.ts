e18e9307f3dc62b5f5f624ca753fb2edffe6fcc8
tahayassine2000najjar-droid <tahayassine2000najjar@gmail.com>
2026-07-15 09:54:51 +0100

    feat: add Book model with schema and integrate zod for validation

---
+++ src/models/Book.ts
@@ -0,0 +1,62 @@
+import mongoose from "mongoose"
+import { boolean } from "zod"
+
+
+export  interface IBookDocument extends mongoose.Document{
+    title :string ,
+    author :string,
+    isbn :string, 
+    category : string,
+    publicationYear : number,
+    description :string,
+    available :boolean , 
+    createdAt :Date , 
+    updatedAt :Date 
+ }
+
+
+ const BookSchema = new mongoose.Schema <IBookDocument>({
+    title: {
+         type : String,
+         required : true ,
+         minlength : 3
+    },
+    author :{
+        type : String ,
+        required : true ,
+       
+    },
+    isbn : {
+         type : String ,
+         required : true ,
+         unique : true 
+    },
+    category : {
+       type : String , 
+       required : true
+    },
+     publicationYear : {
+      type : Number ,
+      required : true
+    },
+    description :  {
+      type : String , 
+      minlength : 10,
+      required : true
+    },
+    available :  {
+      type : Boolean ,
+      default : true
+    },
+     },
+    {
+       timestamps : true ,
+    }
+ )
+
+
+
+ const  Book = mongoose.models.book || mongoose.model<IBookDocument>("Book",BookSchema)
+
+
+ export default Book ; 
