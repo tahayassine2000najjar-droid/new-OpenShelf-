6dd01c83d956c7b75a7cf3341775f43f995c09ec
tahayassine2000najjar-droid <tahayassine2000najjar@gmail.com>
2026-07-16 10:56:54 +0100

    fix: correct error handling and validation in POST request for book creation

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -24,7 +24,34 @@ export async function POST( post : nextRequest){
         const body = await request.json()
         const result = bookSchema.safeParse(body)
         if(!result.success){
-        const errors = result.errors.flatten().fieldErrors    
+        const errors = result.error.flatten().fieldErrors ;
+        return Response.json({errors},{status : 400})    
         }
+        const existingBook = await Book.findOne({isbn: result.data.isbn})
+        if(existingBook){
+            return Response.json(
+                {error : {isbn : ["this isbn is already existing"]}},
+                {status : 400}
+            )
+
+        }
+        const book = await Book.create(result.data)
+        return Response.json(book , {status : 201} )
+    }catch(error : unknown){
+        if (
+            typeof error === "object" && 
+            error !== null &&
+            "code" in error && 
+            (error as {code : number}).code === 11000
+        ){
+            return Response.json(
+                {error : {isbn : ["this isbn is already existing"]}},
+                {status : 400}  
+            )
+        }
+         return Response.json(
+                {error : {isbn : ["this isbn is already existing"]}},
+                {status : 400}
+            )
     }
 }
